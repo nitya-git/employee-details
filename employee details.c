6dd1adbbcb88c31c55e56f1ae15c0e9557be79e4
nitya-git <77236109+nitya-git@users.noreply.github.com>
2021-01-26 20:08:37 +0530

    Add files via upload

---
+++ employee details.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct
+{
+    char name[30],department[30];
+    int no,age,salary;
+} 
+Employee;
+int main()
+{
+    int i, n=3;
+    Employee emp[n];
+    printf("Enter %d Employee Details \n \n",n);
+    for(i=0; i<n; i++)
+	{
+		printf("-----------------\n");
+        printf("Employee %d:\n",i+1);
+        printf("-----------------\n");
+        printf("Name       : ");
+        scanf("%s",&emp[i].name);
+        printf("Emp.no     : ");
+        scanf("%d",&emp[i].no);
+        printf("Age        : ");
+        scanf("%d",&emp[i].age);
+        printf("Department : ");
+        scanf("%s",&emp[i].department);
+        printf("Salary     : ");
+        scanf("%d\n",&emp[i].salary);
+    }
+    printf("-------------------\n");
+    printf("Employees Details\n ------------------\n");
+    for(i=0; i<n; i++)
+	{
+        printf("Name       :%s\n",emp[i].name);
+        printf("Emp.no     :%d \n",emp[i].no);
+        printf("Age        :%d \n",emp[i].age);
+        printf("Department :%s \n",emp[i].department);
+        printf("Salary     :%d \n",emp[i].salary);
+    }
+}
+
+
